9fef83614bbe1fbe4b26654eb5e50578b7d9d1d7
Baptiste Boimard <bouketin27@gmail.com>
2023-11-17 16:20:42 +0100

    essai avec watchtower et sans deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Powa2222{' '}
+          Powa{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
